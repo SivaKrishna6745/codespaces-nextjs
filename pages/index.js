ea20241faa1cfb56a04b7d8fae29ee4fb3e2b2f0
Siva Krishna <krishnasiva6745@gmail.com>
2026-06-07 08:35:31 +0000

    feat: Implement the index page

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,65 @@
-import { useCallback, useEffect, useState } from 'react'
-import Button from '../components/Button'
-import ClickCount from '../components/ClickCount'
-import styles from '../styles/home.module.css'
-
-function throwError() {
-  console.log(
-    // The function body() is not defined
-    document.body()
-  )
-}
+import { useCallback, useEffect, useState } from "react";
+import Button from "../components/Button";
+import ClickCount from "../components/ClickCount";
+import styles from "../styles/home.module.css";
+import useProfile from "../store/useProfileStore";
+import ProfileHeader from "../components/profileHeader";
+import RepoCard from "../components/RepoCard";
+import StatsChart from "../components/StatsChart";
 
 function Home() {
-  const [count, setCount] = useState(0)
-  const increment = useCallback(() => {
-    setCount((v) => v + 1)
-  }, [setCount])
+  const { profile, repos, isLoading, error, fetchGithubData } = useProfile();
+  const [username, setUsername] = useState("");
 
-  useEffect(() => {
-    const r = setInterval(() => {
-      increment()
-    }, 1000)
-
-    return () => {
-      clearInterval(r)
-    }
-  }, [increment])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchGithubData(username);
+  };
 
   return (
-    <main className={styles.main}>
-      <h1>Fast Refresh Demo</h1>
-      <p>
-        Fast Refresh is a Next.js feature that gives you instantaneous feedback
-        on edits made to your React components, without ever losing component
-        state.
-      </p>
-      <hr className={styles.hr} />
-      <div>
-        <p>
-          Auto incrementing value. The counter won't reset after edits or if
-          there are errors.
-        </p>
-        <p>Current value: {count}</p>
-      </div>
-      <hr className={styles.hr} />
-      <div>
-        <p>Component with state.</p>
-        <ClickCount />
-      </div>
-      <hr className={styles.hr} />
-      <div>
-        <p>
-          The button below will throw 2 errors. You'll see the error overlay to
-          let you know about the errors but it won't break the page or reset
-          your state.
-        </p>
-        <Button
-          onClick={(e) => {
-            setTimeout(() => document.parentNode(), 0)
-            throwError()
-          }}
+    <div className="p-2 max-w-7xl mx-auto">
+      <h1 className="text-3xl text-center my-4 uppercase">
+        Search GitHub Details of a User
+      </h1>
+      <div className="h-0.5 bg-gray-800 relative left-1/2 right-1/2 ml-[-50vw] mr-[-50vw] w-screen"></div>
+      <form
+        action=""
+        onSubmit={handleSubmit}
+        className="flex gap-4 justify-center items-center my-4"
+      >
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Enter Github Username"
+          className="w-md px-1 py-2 border-2 rounded-sm border-green-400"
+        />
+        <button
+          type="submit"
+          className="px-5 py-2 rounded-sm cursor-pointer bg-blue-500 text-white text-lg hover:bg-blue-600/80 active:scale-95 active:shadow-lg duration-200 transition-all"
         >
-          Throw an Error
-        </Button>
+          Get Details
+        </button>
+      </form>
+      {isLoading && (
+        <div className="text-sm text-blue-500">Loading, please wait!!</div>
+      )}
+      {error && (
+        <div className="text-sm text-red-500">
+          Error occurred, please try again
+        </div>
+      )}
+      <div className="flex flex-col gap-6">
+        {profile && (
+          <>
+            <ProfileHeader profile={profile} />
+            <RepoCard repos={repos} name={profile.login} />
+            <StatsChart />
+          </>
+        )}
       </div>
-      <hr className={styles.hr} />
-    </main>
-  )
+    </div>
+  );
 }
 
-export default Home
+export default Home;
